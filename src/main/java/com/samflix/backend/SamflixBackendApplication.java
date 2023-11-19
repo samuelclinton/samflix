0e4ee3b66d6b908de04a2ef21eba4b318ed20581
samuelclinton <contato@samuelclinton.com>
2023-11-19 02:48:16 -0300

    Criação do projeto

---
+++ src/main/java/com/samflix/backend/SamflixBackendApplication.java
@@ -0,0 +1,13 @@
+package com.samflix.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamflixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamflixBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samflix/backend/SamflixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samflix.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamflixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
