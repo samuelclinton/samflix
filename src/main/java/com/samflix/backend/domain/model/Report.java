7ddb8c5627529b04dc167281df5df25d51d63f9d
samuelclinton <contato@samuelclinton.com>
2023-11-19 03:15:54 -0300

    Criação de uma classe para representar o relatório de estatísticas

---
+++ src/main/java/com/samflix/backend/domain/model/Report.java
@@ -0,0 +1,11 @@
+package com.samflix.backend.domain.model;
+
+public class Report {
+
+    private Long videos;
+    private Long likes;
+    private Long averageLikes;
+    private Long views;
+    private Long averageViews;
+
+}
