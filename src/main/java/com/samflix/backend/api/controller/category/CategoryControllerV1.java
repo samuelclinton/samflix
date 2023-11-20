017e7c391974b6322dd0e7369572a0023787121e
samuelclinton <contato@samuelclinton.com>
2023-11-20 15:06:36 -0300

    Adição da documentação dos parâmetros no controller de categorias

--- src/main/java/com/samflix/backend/api/controller/category/CategoryControllerV1.java
+++ src/main/java/com/samflix/backend/api/controller/category/CategoryControllerV1.java
@@ -2,9 +2,12 @@ package com.samflix.backend.api.controller.category;
 
 import com.samflix.backend.domain.model.Category;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import static com.samflix.backend.utils.OpenApiSchemaExamples.UUID_EXAMPLE;
 
 @Tag(name = "Categories")
 public interface CategoryControllerV1 {
@@ -19,7 +22,7 @@ public interface CategoryControllerV1 {
             summary = "Atualiza uma categoria",
             description = "Atualiza uma categoria"
     )
-    Category update(String categoryId);
+    Category update(@Parameter(description = "O ID de uma categoria", example = UUID_EXAMPLE) String categoryId);
 
     @Operation(
             summary = "Lista as categorias",
@@ -31,6 +34,6 @@ public interface CategoryControllerV1 {
             summary = "Exclui uma categoria",
             description = "Exclui uma categoria"
     )
-    void delete(String categoryId);
+    void delete(@Parameter(description = "O ID de uma categoria", example = UUID_EXAMPLE) String categoryId);
 
 }
