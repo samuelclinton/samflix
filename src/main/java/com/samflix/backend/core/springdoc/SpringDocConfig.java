ee39ec76d61b799d3f08a18e59a9a608e9aed6c8
samuelclinton <contato@samuelclinton.com>
2023-11-20 14:47:12 -0300

    Adição e configuração do SpringDoc no projeto

---
+++ src/main/java/com/samflix/backend/core/springdoc/SpringDocConfig.java
@@ -0,0 +1,32 @@
+package com.samflix.backend.core.springdoc;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.tags.Tag;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        var tags = new ArrayList<Tag>();
+        tags.add(new Tag().name("Videos").description("Gerencia os vídeos"));
+        tags.add(new Tag().name("Categories").description("Gerencia as categorias"));
+        tags.add(new Tag().name("Users").description("Gerencia os usuários"));
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Samflix")
+                        .version("v1")
+                        .description("API da aplicação de vídeos Samflix"))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Repositório")
+                        .url("https://github.com/samuelclinton/samflix"))
+                .tags(tags);
+    }
+
+}
